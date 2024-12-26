295378daf3396710c869d3f44a7bc212289c0e9b
Mihir K <mihir@soulhq.ai>
2024-12-26 13:42:42 +0530

    renamed file to avoid error due to case

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import './styles/index.scss'
-import App from './App.tsx'
+import "./styles/index.scss";
+import App from "./applet.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
